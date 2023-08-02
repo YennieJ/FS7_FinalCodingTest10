e1998e36fbc9c2ef09f1bcf0e18bbb9d32ed3748
yen <l.yennie.j@gmail.com>
2023-08-02 14:44:26 +0900

    Main 마크업

--- 01.junglecinema/src/components/main/Main.jsx
+++ 01.junglecinema/src/components/main/Main.jsx
@@ -3,21 +3,41 @@ import data from "data/data.json";
 import "./Main.css";
 const Main = () => {
   return (
-    <>
-      {data.map((item) => (
-        <div key={item.id}>
-          <h1>{item.title}</h1>
-          <img src={require(`${item.poster}`)} alt="" />
+    <div>
+      <header>
+        <h2>정글 시네마 영화 목록</h2>
+        <nav>
+          <ul>
+            <li>
+              <button>현재상영영화</button>
+            </li>
+            <li>
+              <button>개봉예정영화</button>
+            </li>
+            <li>
+              <button>박스오피스</button>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <article>
+        <ul>
+          {data.map((item) => (
+            <li key={item.id}>
+              <h2>{item.title}</h2>
+              <img src={require(`${item.poster}`)} alt="" />
 
-          <span>{item.genre}</span>
-          <span>{item.star}</span>
-          <span>{item.release}</span>
-          <span>{item.actor}</span>
+              <span>{item.genre}</span>
+              <span>{item.star}</span>
+              <span>{item.release}</span>
+              <span>{item.actor}</span>
 
-          <a href={item.url}>예매하기</a>
-        </div>
-      ))}
-    </>
+              <a href={item.url}>예매하기</a>
+            </li>
+          ))}
+        </ul>
+      </article>
+    </div>
   );
 };
 
