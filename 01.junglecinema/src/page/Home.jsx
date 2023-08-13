a36ecd7128ec127a13bb214ec3d89300e7574c0a
yen <l.yennie.j@gmail.com>
2023-08-14 02:14:47 +0900

    안쓰는 파일 정리

--- 01.junglecinema/src/page/Home.jsx
+++ 01.junglecinema/src/page/Home.jsx
@@ -3,13 +3,12 @@ import React from "react";
 import Main from "../components/main/Main";
 import Footer from "../components/footer/Footer";
 
-import "./Home.css";
 const Home = () => {
   return (
-    <div className="home_container">
+    <>
       <Main />
-      {/* <Footer /> */}
-    </div>
+      <Footer />
+    </>
   );
 };
 
