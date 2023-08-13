1ec27caca46667642b4ee3b58088257e198aa049
yen <l.yennie.j@gmail.com>
2023-08-14 00:43:19 +0900

    파이널 테스트 1. 버튼 엑티브, 데이터 변화

--- 01.junglecinema/src/components/main/Main.jsx
+++ 01.junglecinema/src/components/main/Main.jsx
@@ -1,33 +1,58 @@
-import React from "react";
-import data from "data/data.json";
+import React, { useState } from "react";
+import movieJson from "data/data.json";
+import movieReverseJson from "data/dataReverse.json";
+
 import "./Main.css";
 const Main = () => {
+  // jsonData
+  const [movieData, setMovieData] = useState(movieJson);
+  // nav button
+  const movieTyepButton = ["현재상영영화", "개봉예정영화", "박스오피스"];
+
+  // 어떤 nav button을 사용중인지를 알기 위한 useState와 함수
+  const [buttonActive, setButtonActive] = useState("현재상영영화");
+  const activeMovie = (e) => {
+    const activeTitle = e.target.innerText;
+
+    if (activeTitle === "현재상영영화") {
+      setMovieData(movieJson);
+      setButtonActive("현재상영영화");
+    } else if (activeTitle === "개봉예정영화") {
+      setMovieData(movieReverseJson);
+      setButtonActive("개봉예정영화");
+    } else {
+      setMovieData(movieJson);
+      setButtonActive("박스오피스");
+    }
+  };
+
   return (
     <div className="main_container">
       <header className="main_header">
-        <h2 className="main_title">정글 시네마 영화 목록</h2>
+        <h1 className="main_title">정글 시네마 영화 목록</h1>
         <nav>
           <ul className="main_nav">
-            <li>
-              <button>현재상영영화</button>
-            </li>
-            <li>
-              <button>개봉예정영화</button>
-            </li>
-            <li>
-              <button>박스오피스</button>
-            </li>
+            {movieTyepButton.map((item) => (
+              <button
+                onClick={activeMovie}
+                key={item}
+                className={item === buttonActive ? " button_active" : ""}
+              >
+                {item}
+              </button>
+            ))}
           </ul>
         </nav>
       </header>
       <article className="main_content">
         <ul className="main_grid">
-          {data.map((item) => (
+          <h2 className="a11y-hidden">{buttonActive}</h2>
+          {movieData.map((item) => (
             <li key={item.id} className="movie_item">
               <img
                 className="movie_poster"
                 src={require(`${item.poster}`)}
-                alt={`${item.title} 포스터₩`}
+                alt={`${item.title} 포스터`}
               />
               <h2 className="movie_name">{item.title}</h2>
               <div className="movie_detail">
